0da62cf0474ed89ce18298930d11e87e2ecd6c54
Praveen K M <99317905+PKM777@users.noreply.github.com>
2023-01-06 22:17:57 +0530

    create

---
+++ redux-pro/src/comps/Form.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react"
+import { submitForm } from "../features/objSlice"
+import { useDispatch } from "react-redux"
+
+const Form = ()=>{
+    
+    const [name, setName] = useState('')
+    const [job, setJob] = useState('')
+    const [exp, setExp] = useState(0)
+    const dispatch = useDispatch()
+
+    const nameChange = (e)=>{
+        setName(e.target.value)
+    }
+    const jobChange = (e)=>{
+        setJob(e.target.value)
+    }
+    const expChange = (e)=>{
+        setExp(e.target.value)
+    }
+
+    const onsubmit = (e)=>{
+        e.preventDefault()
+        dispatch(submitForm(
+            
+            name,
+            job,
+            exp
+        ))
+    }
+
+    return (
+        <div>
+            <form onSubmit={onsubmit}>
+                <label htmlFor="name">Name</label>
+                <input onChange={nameChange} value={name} type="text" name="name"/>
+
+                <label htmlFor="job">Job</label>
+                <input onChange={jobChange}  value={job} name="job" type="text" />
+
+                <label htmlFor="exp">Exp</label>
+                <input onChange={expChange}  value={exp} name="exp" type="number" />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+
+export default Form
