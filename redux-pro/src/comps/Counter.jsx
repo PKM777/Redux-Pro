a8ed4153fb2dc0886961551b5574c22a36edb193
Praveen K M <99317905+PKM777@users.noreply.github.com>
2023-01-06 22:18:51 +0530

    update

--- redux-pro/src/comps/Counter.jsx
+++ redux-pro/src/comps/Counter.jsx
@@ -1,18 +1,45 @@
 
+import { useState } from 'react'
 import { useSelector , useDispatch } from 'react-redux'
-import { increment } from '../features/counterSlice'
+import { increment, incrementByValue } from '../features/counterSlice'
+import { allObjs } from '../features/objSlice'
+
 
 const Counter = () => {
-      
+
+    const [bar, setbar] = useState(0)
+    
     const count = useSelector((state)=> state.counter.count)
+    
+ 
     const dispatch = useDispatch()
+    const posts = useSelector(allObjs)
+    const addValue = Number(bar) | 0
+    const barSet = (e)=>{
+      setbar(e.target.value)
+    }
+
+    const renderChars = posts.map((items)=>(
+      <div key={items.id}>
+        <h1>{items.name}</h1>
+        <h2>{items.job}</h2>
+        <h3>{items.exp}</h3>
+      </div>
+    ))
 
   return (
     <div>
         <h1>{count}</h1>
+        <input value={bar} onChange={(e)=>barSet(e)} />
+        <button onClick={()=> dispatch(incrementByValue(addValue))}>Increment By Value</button>
         <button
-         onClick={(e)=>dispatch(increment(e))}
+         onClick={()=>dispatch(increment())}
          >Increment</button>
+
+         <div>
+          {renderChars}
+         </div>
+
     </div>
   )
 }
